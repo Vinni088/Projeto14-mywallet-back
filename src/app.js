8897885c2dffd9541d074142b2a0b278f2e87ec9
Vinicius Evangelista <viniciusevangelista088@gmail.comgit config --global user.name Vinni>
2023-07-09 23:20:16 -0300

    Checking for bugs1

--- src/app.js
+++ src/app.js
@@ -8,5 +8,5 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
-const PORT = process.env.PORT || 
+const PORT = process.env.PORT 
 app.listen(PORT, () => console.log(`Servidor está rodando na porta ${PORT}`))
